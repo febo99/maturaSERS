bd95049f229a1193afd45c1ec12a6556f16ddb3e
Žiga Lah <febo.89.89@gmail.com>
2019-06-23 19:38:57 +0200

    Delete povezavaBaza.php

--- povezavaBaza.php
+++
@@ -1,9 +0,0 @@
-<?php
-  define("naslov", 'localhost');
-  define("baza", 'stavnica');
-  define("uporabnik",'root');
-  define("geslo",'rdecastrela6');
-
-  $povezava = mysqli_connect(naslov,uporabnik,geslo) or die("Povezava ni uspela".mysqli_error($povezava));
-  $izbranaBaza = mysqli_select_db($povezava,'stavnica') or die("Povezava ni uspela".mysqli_error($povezava));
-?>
